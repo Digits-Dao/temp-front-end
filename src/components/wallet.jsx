ffebf857e8e39b9b8c35d279a26ec586992adc2e
Ric Waltman <ric.waltman@protonmail.com>
2023-02-02 14:18:58 -0700

    Only Static Files Now

--- src/components/wallet.jsx
+++
@@ -1,4 +0,0 @@
-// w3m-core-button is a web-component button that reacts to modal's changes
-export default function WalletButton() {
-  return <w3m-core-button icon="hide" balance="show"></w3m-core-button>;
-}
