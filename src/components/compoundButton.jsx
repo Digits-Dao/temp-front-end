b78ba848b4258ceea54a7ba608c6de6cfe7c9cff
Ricky on the Block <rickyontheblock@pm.me>
2023-02-02 21:06:30 -0700

    Add Disabled Button Mouseovers

--- src/components/compoundButton.jsx
+++ src/components/compoundButton.jsx
@@ -70,6 +70,7 @@ export default function CompoundButton() {
         class="relative inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
         onClick={(e) => setClickEvent(e)}
         disabled={!hasClaimableDAI()}
+        title={hasClaimableDAI() ? '' : 'No DAI to compound'}
       >
         Compound DAI &rarr; DIGITS
       </button>
